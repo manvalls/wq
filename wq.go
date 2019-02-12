8491ea82ab59e95788c08eab16bbedfb5aebb511
Manuel Valls Fernández <manolo@vlrz.es>
2019-02-12 21:27:40 +0100

    Rename Action to Command

--- wq.go
+++ wq.go
@@ -18,51 +18,51 @@ var Body = S("body").First()
 // Title matches the document title
 var Title = S("head > title").First()
 
-// Action wraps a set of actions and wrappers
-type Action struct {
+// Command wraps a set of commands and wrappers
+type Command struct {
 	mux     sync.Mutex
 	delta   *wit.Delta
-	parent  *Action
-	action  wit.Action
-	wrapper func(wit.Action) wit.Action
+	parent  *Command
+	command wit.Command
+	wrapper func(wit.Command) wit.Command
 }
 
 // Delta returns the resolved delta
-func (action *Action) Delta() wit.Delta {
-	action.mux.Lock()
-	defer action.mux.Unlock()
+func (command *Command) Delta() wit.Delta {
+	command.mux.Lock()
+	defer command.mux.Unlock()
 
-	if action.delta != nil {
-		return *action.delta
+	if command.delta != nil {
+		return *command.delta
 	}
 
-	root := action
-	actions := []wit.Action{}
+	root := command
+	commands := []wit.Command{}
 
-	for action != nil {
-		if action.wrapper != nil {
-			actions = []wit.Action{action.wrapper(wit.List(actions...))}
+	for command != nil {
+		if command.wrapper != nil {
+			commands = []wit.Command{command.wrapper(wit.List(commands...))}
 		} else {
-			actions = append(actions, action.action)
+			commands = append(commands, command.command)
 		}
 
-		action = action.parent
+		command = command.parent
 	}
 
-	delta := wit.List(actions...).Delta()
+	delta := wit.List(commands...).Delta()
 	root.delta = &delta
 	return delta
 }
 
 // Procedure returns a procedure wrapping the resolved delta
-func (action *Action) Procedure() wok.Procedure {
-	return wok.Action(action).Procedure()
+func (command *Command) Procedure() wok.Procedure {
+	return wok.Command(command).Procedure()
 }
 
 // Selector wraps a CSS selector
 type Selector struct {
 	selector wit.Selector
-	*Action
+	*Command
 }
 
 // S builds a wrapper based on the provided selector
@@ -70,267 +70,267 @@ func S(selector string) Selector {
 	s := wit.S(selector)
 	return Selector{
 		selector: s,
-		Action: &Action{
-			wrapper: func(action wit.Action) wit.Action {
-				return s.All(action)
+		Command: &Command{
+			wrapper: func(command wit.Command) wit.Command {
+				return s.All(command)
 			},
 		},
 	}
 }
 
 // First matches only the first element
-func (s Selector) First() *Action {
-	return &Action{
-		wrapper: func(action wit.Action) wit.Action {
-			return s.selector.One(action)
+func (s Selector) First() *Command {
+	return &Command{
+		wrapper: func(command wit.Command) wit.Command {
+			return s.selector.One(command)
 		},
 	}
 }
 
-// Apply applies the given actions
-func (action *Action) Apply(actions ...wit.Action) *Action {
-	return &Action{
-		parent: action,
-		action: wit.List(actions...),
+// Apply applies the given commands
+func (command *Command) Apply(commands ...wit.Command) *Command {
+	return &Command{
+		parent:  command,
+		command: wit.List(commands...),
 	}
 }
 
 // Root matches the root element
-func (action *Action) Root() *Action {
-	return &Action{
-		parent: action,
-		wrapper: func(action wit.Action) wit.Action {
-			return wit.Root(action)
+func (command *Command) Root() *Command {
+	return &Command{
+		parent: command,
+		wrapper: func(command wit.Command) wit.Command {
+			return wit.Root(command)
 		},
 	}
 }
 
 // Parent matches the parents of matched elements
-func (action *Action) Parent() *Action {
-	return &Action{
-		parent: action,
-		wrapper: func(action wit.Action) wit.Action {
-			return wit.Parent(action)
+func (command *Command) Parent() *Command {
+	return &Command{
+		parent: command,
+		wrapper: func(command wit.Command) wit.Command {
+			return wit.Parent(command)
 		},
 	}
 }
 
 // FirstChild matches the first child of matched elements
-func (action *Action) FirstChild() *Action {
-	return &Action{
-		parent: action,
-		wrapper: func(action wit.Action) wit.Action {
-			return wit.FirstChild(action)
+func (command *Command) FirstChild() *Command {
+	return &Command{
+		parent: command,
+		wrapper: func(command wit.Command) wit.Command {
+			return wit.FirstChild(command)
 		},
 	}
 }
 
 // LastChild matches the last child of matched elements
-func (action *Action) LastChild() *Action {
-	return &Action{
-		parent: action,
-		wrapper: func(action wit.Action) wit.Action {
-			return wit.LastChild(action)
+func (command *Command) LastChild() *Command {
+	return &Command{
+		parent: command,
+		wrapper: func(command wit.Command) wit.Command {
+			return wit.LastChild(command)
 		},
 	}
 }
 
 // PrevSibling matches the previous sibling of matched elements
-func (action *Action) PrevSibling() *Action {
-	return &Action{
-		parent: action,
-		wrapper: func(action wit.Action) wit.Action {
-			return wit.PrevSibling(action)
+func (command *Command) PrevSibling() *Command {
+	return &Command{
+		parent: command,
+		wrapper: func(command wit.Command) wit.Command {
+			return wit.PrevSibling(command)
 		},
 	}
 }
 
 // NextSibling matches the next sibling of matched elements
-func (action *Action) NextSibling() *Action {
-	return &Action{
-		parent: action,
-		wrapper: func(action wit.Action) wit.Action {
-			return wit.NextSibling(action)
+func (command *Command) NextSibling() *Command {
+	return &Command{
+		parent: command,
+		wrapper: func(command wit.Command) wit.Command {
+			return wit.NextSibling(command)
 		},
 	}
 }
 
 // Remove removes matched elements
-func (action *Action) Remove() *Action {
-	return &Action{
-		parent: action,
-		action: wit.Remove,
+func (command *Command) Remove() *Command {
+	return &Command{
+		parent:  command,
+		command: wit.Remove,
 	}
 }
 
 // Clear empties matched elements
-func (action *Action) Clear() *Action {
-	return &Action{
-		parent: action,
-		action: wit.Clear,
+func (command *Command) Clear() *Command {
+	return &Command{
+		parent:  command,
+		command: wit.Clear,
 	}
 }
 
 // Set sets the contents of matched elements
-func (action *Action) Set(factory wit.Factory) *Action {
-	return &Action{
-		parent: action,
-		action: wit.HTML(factory),
+func (command *Command) Set(factory wit.Factory) *Command {
+	return &Command{
+		parent:  command,
+		command: wit.HTML(factory),
 	}
 }
 
 // SetText sets the text content of matched elements
-func (action *Action) SetText(text string) *Action {
-	return action.Set(wit.FromString(html.EscapeString(text)))
+func (command *Command) SetText(text string) *Command {
+	return command.Set(wit.FromString(html.EscapeString(text)))
 }
 
 // SetHTML sets the HTML content of matched elements
-func (action *Action) SetHTML(text string) *Action {
-	return action.Set(wit.FromString(text))
+func (command *Command) SetHTML(text string) *Command {
+	return command.Set(wit.FromString(text))
 }
 
 // Replace replaces matching elements
-func (action *Action) Replace(factory wit.Factory) *Action {
-	return &Action{
-		parent: action,
-		action: wit.Replace(factory),
+func (command *Command) Replace(factory wit.Factory) *Command {
+	return &Command{
+		parent:  command,
+		command: wit.Replace(factory),
 	}
 }
 
 // ReplaceText replaces matching elements with the provided text
-func (action *Action) ReplaceText(text string) *Action {
-	return action.Replace(wit.FromString(html.EscapeString(text)))
+func (command *Command) ReplaceText(text string) *Command {
+	return command.Replace(wit.FromString(html.EscapeString(text)))
 }
 
 // ReplaceHTML replaces matching elements with the provided HTML
-func (action *Action) ReplaceHTML(text string) *Action {
-	return action.Replace(wit.FromString(text))
+func (command *Command) ReplaceHTML(text string) *Command {
+	return command.Replace(wit.FromString(text))
 }
 
 // Append appends content to matched elements
-func (action *Action) Append(factory wit.Factory) *Action {
-	return &Action{
-		parent: action,
-		action: wit.Append(factory),
+func (command *Command) Append(factory wit.Factory) *Command {
+	return &Command{
+		parent:  command,
+		command: wit.Append(factory),
 	}
 }
 
 // AppendText appends text to elements
-func (action *Action) AppendText(text string) *Action {
-	return action.Append(wit.FromString(html.EscapeString(text)))
+func (command *Command) AppendText(text string) *Command {
+	return command.Append(wit.FromString(html.EscapeString(text)))
 }
 
 // AppendHTML appends HTML to matched elements
-func (action *Action) AppendHTML(text string) *Action {
-	return action.Append(wit.FromString(text))
+func (command *Command) AppendHTML(text string) *Command {
+	return command.Append(wit.FromString(text))
 }
 
 // Prepend prepends content to matched elements
-func (action *Action) Prepend(factory wit.Factory) *Action {
-	return &Action{
-		parent: action,
-		action: wit.Prepend(factory),
+func (command *Command) Prepend(factory wit.Factory) *Command {
+	return &Command{
+		parent:  command,
+		command: wit.Prepend(factory),
 	}
 }
 
 // PrependText prepends text to elements
-func (action *Action) PrependText(text string) *Action {
-	return action.Prepend(wit.FromString(html.EscapeString(text)))
+func (command *Command) PrependText(text string) *Command {
+	return command.Prepend(wit.FromString(html.EscapeString(text)))
 }
 
 // PrependHTML prepends HTML to matched elements
-func (action *Action) PrependHTML(text string) *Action {
-	return action.Prepend(wit.FromString(text))
+func (command *Command) PrependHTML(text string) *Command {
+	return command.Prepend(wit.FromString(text))
 }
 
 // InsertAfter inserts content after matched elements
-func (action *Action) InsertAfter(factory wit.Factory) *Action {
-	return &Action{
-		parent: action,
-		action: wit.InsertAfter(factory),
+func (command *Command) InsertAfter(factory wit.Factory) *Command {
+	return &Command{
+		parent:  command,
+		command: wit.InsertAfter(factory),
 	}
 }
 
 // InsertTextAfter inserts text after elements
-func (action *Action) InsertTextAfter(text string) *Action {
-	return action.InsertAfter(wit.FromString(html.EscapeString(text)))
+func (command *Command) InsertTextAfter(text string) *Command {
+	return command.InsertAfter(wit.FromString(html.EscapeString(text)))
 }
 
 // InsertHTMLAfter inserts HTML after matched elements
-func (action *Action) InsertHTMLAfter(text string) *Action {
-	return action.InsertAfter(wit.FromString(text))
+func (command *Command) InsertHTMLAfter(text string) *Command {
+	return command.InsertAfter(wit.FromString(text))
 }
 
 // InsertBefore inserts content before matched elements
-func (action *Action) InsertBefore(factory wit.Factory) *Action {
-	return &Action{
-		parent: action,
-		action: wit.InsertBefore(factory),
+func (command *Command) InsertBefore(factory wit.Factory) *Command {
+	return &Command{
+		parent:  command,
+		command: wit.InsertBefore(factory),
 	}
 }
 
 // InsertTextBefore inserts text before elements
-func (action *Action) InsertTextBefore(text string) *Action {
-	return action.InsertBefore(wit.FromString(html.EscapeString(text)))
+func (command *Command) InsertTextBefore(text string) *Command {
+	return command.InsertBefore(wit.FromString(html.EscapeString(text)))
 }
 
 // InsertHTMLBefore inserts HTML before matched elements
-func (action *Action) InsertHTMLBefore(text string) *Action {
-	return action.InsertBefore(wit.FromString(text))
+func (command *Command) InsertHTMLBefore(text string) *Command {
+	return command.InsertBefore(wit.FromString(text))
 }
 
 // AddAttr adds the provided attributes to the matching elements
-func (action *Action) AddAttr(attr map[string]string) *Action {
-	return &Action{
-		parent: action,
-		action: wit.AddAttr(attr),
+func (command *Command) AddAttr(attr map[string]string) *Command {
+	return &Command{
+		parent:  command,
+		command: wit.AddAttr(attr),
 	}
 }
 
 // SetAttr sets the attributes of the matching elements
-func (action *Action) SetAttr(attr map[string]string) *Action {
-	return &Action{
-		parent: action,
-		action: wit.SetAttr(attr),
+func (command *Command) SetAttr(attr map[string]string) *Command {
+	return &Command{
+		parent:  command,
+		command: wit.SetAttr(attr),
 	}
 }
 
 // RmAttr removes the provided attributes from the matching elements
-func (action *Action) RmAttr(attrs ...string) *Action {
-	return &Action{
-		parent: action,
-		action: wit.RmAttr(attrs...),
+func (command *Command) RmAttr(attrs ...string) *Command {
+	return &Command{
+		parent:  command,
+		command: wit.RmAttr(attrs...),
 	}
 }
 
 // AddStyles adds the provided styles to the matching elements
-func (action *Action) AddStyles(styles map[string]string) *Action {
-	return &Action{
-		parent: action,
-		action: wit.AddStyles(styles),
+func (command *Command) AddStyles(styles map[string]string) *Command {
+	return &Command{
+		parent:  command,
+		command: wit.AddStyles(styles),
 	}
 }
 
 // RmStyles removes the provided styles from the matching elements
-func (action *Action) RmStyles(styles ...string) *Action {
-	return &Action{
-		parent: action,
-		action: wit.RmStyles(styles...),
+func (command *Command) RmStyles(styles ...string) *Command {
+	return &Command{
+		parent:  command,
+		command: wit.RmStyles(styles...),
 	}
 }
 
 // AddClass adds the provided class to the matching elements
-func (action *Action) AddClass(class string) *Action {
-	return &Action{
-		parent: action,
-		action: wit.AddClass(class),
+func (command *Command) AddClass(class string) *Command {
+	return &Command{
+		parent:  command,
+		command: wit.AddClass(class),
 	}
 }
 
 // RmClass removes the provided class from the matching elements
-func (action *Action) RmClass(class string) *Action {
-	return &Action{
-		parent: action,
-		action: wit.RmClass(class),
+func (command *Command) RmClass(class string) *Command {
+	return &Command{
+		parent:  command,
+		command: wit.RmClass(class),
 	}
 }
